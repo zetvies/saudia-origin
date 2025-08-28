60bd008b0c77443c44e41d3c961b42b90c1dd938
Bimo Arsa <zetvies@gmail.com>
2025-08-29 01:56:02 +0700

    submit to database

---
+++ public/config.js
@@ -0,0 +1,4 @@
+const config = {
+    supabaseUrl: "https://fowdecmskhvfgmdwwnxg.supabase.co",
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvd2RlY21za2h2ZmdtZHd3bnhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE1MTk2MjUsImV4cCI6MjAxNzA5NTYyNX0.icC5QydnHRC-qu4Vf0Ik0_BxpaU39TCaU8bXijdqv9Y"
+};
